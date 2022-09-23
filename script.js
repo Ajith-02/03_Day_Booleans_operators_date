74abce8c79cd142d9de92200e63329e1a40784b5
pajith551@gmail.com <pajith551@gmail.com>
2022-09-23 12:10:17 +0530

    day 3

---
+++ script.js
@@ -0,0 +1,31 @@
+let isLightOn = true;
+let isRaining = false;
+let isHungry = false;
+let trueValue = 4 > 3;
+let falseValue = 4 < 3;
+
+let un1;
+console.log(un1);
+let un2;
+console.log(un2);
+let un3;
+console.log(un3);
+
+let empty1 = null;
+console.log(empty1);
+console.log(typeof(empty1));
+let empty2 = null;
+console.log(empty2);
+let empty3 = null;
+console.log(empty3);
+
+let firstName1 = "Alex";
+let lastName1 = "Z";
+
+// = x=y x=y
+// += x+=y x=x+y
+// -= x-=y x=x-y
+// *= x*=y x=x*y
+// /= x/=y x=x/y
+// %= x%=y x=x%y
+// **= x**=y x=x**y
